046fedf0f39dc0456ebdc4e366d4f2de88b3a68d
B353N <r.slavov.work@gmail.com>
2023-10-08 14:38:23 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ class Main {
 
         System.out.println("What is your name ?");
         String name = scanner.nextLine();
+        System.out.println("How old are you ?");
+        String age = scanner.nextLine();
 
-        System.out.println("Hello " + name);
+        System.out.println("Hello " + name + " You are " + age + " years old!");
     }
 }
